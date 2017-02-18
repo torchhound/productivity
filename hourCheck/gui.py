14c96a35bcbc22dc93cc2859f4dc6f500cd376ec
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-02-18 15:29:59 -0500

    Added hourCheck/gui, launches a window and logs text input to a text file, learned that tkinter is not thread safe

---
+++ hourCheck/gui.py
@@ -0,0 +1,36 @@
+from time import sleep
+from threading import Thread
+from tkinter import *
+
+def submitText():
+	f = open("hourCheck.txt", "a")
+	f.write(textEntry.get() + "\n")
+	textEntry.delete(0,END)
+
+def hourCheck():
+	while(True):
+		sleep(3600)
+		root.lift()
+		print("Lifted!")
+
+def hourCheckThread():
+	thread = Thread(target=hourCheck)
+	thread.start()
+	thread.join()
+
+root = Tk()
+root.geometry("400x400")
+frame = Frame(root)
+frame.pack()
+
+bottomFrame = Frame(root)
+bottomFrame.pack( side = BOTTOM )
+
+textEntry = Text(root, height=20, width=40)
+textEntry.pack( side = TOP )
+
+blackButton = Button(bottomFrame, text="Submit", fg="black", command=submitText)
+blackButton.pack( side = BOTTOM)
+
+#root.after(1000, hourCheckThread)
+root.mainloop()
